26a6f426c2f98bd973963425cc3adcc562bcef37
vilyever <250450035@163.com>
2016-08-05 17:18:28 +0800

    first release

---
+++ README.md
@@ -0,0 +1,17 @@
+# objcSelectorArgument
+objcSelectorArgument
+
+## Import
+### [CocoaPods](http://cocoapods.org)
+use_frameworks!
+</br>
+pod 'objcSelectorArgument'
+
+### [Carthage](https://github.com/Carthage/Carthage)
+github "vilyever/objcSelectorArgument" "master"
+
+## Usage
+
+## License
+
+[MIT license](LICENSE)
--- objcSelectorArgument/objcSelectorArgument.h
+++ objcSelectorArgument/objcSelectorArgument.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char objcSelectorArgumentVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <objcSelectorArgument/PublicHeader.h>
 
-
+#import <objcSelectorArgument/VDSelectorArgument.h>
+#import <objcSelectorArgument/NSObject+VDSelectorArgument.h>
